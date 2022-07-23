de3079d1b0e814b3ad01ca69ce5a2e03f98835f9
Daveside9 <daveside00468@gmail.com>
2022-07-23 10:51:12 -0700

    Test

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,2 +1,12 @@
-nt _putchar(char c);
+#ifndef main.h
+#define main.h
 
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+
+#endif /*_main_H_ */
