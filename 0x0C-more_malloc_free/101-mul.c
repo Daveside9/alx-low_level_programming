3616e6e11f2e0c312bfeec822e585adf3ffcef7e
Daveside9 <daveside00468@gmail.com>
2022-07-28 16:35:21 -0700

    Test

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument for the function
+ * @argv: argument for the function
+ *
+ * Return: 0.
+ */
+int main(int argc, char **argv)
+{
+int mult = 0;
+
+if (argc == 3)
+{
+	mult = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mult);
+}
+else
+{
+	printf("Error\n");
+	exit(98);
+}
+return (0);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: argument for the function
+ * @max: argument for the function
+ *
+ * Return: an array of integers
+ */
+int *array_range(int min, int max)
+{
+int *array;
+int i, total;
+
+if (min > max)
+	return (NULL);
+total = max - min;
+array = malloc((total + 1) * sizeof(int));
+if (array == NULL)
+{
+	free(array);
+	return (NULL);
+}
+for (i = 0; i <= total; i++)
+{
+	array[i] = min;
+	min++;
+}
+return (array);
+}
