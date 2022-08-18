996f27112f98dcb882862b9c902300cd209ad913
Arnyfavy <daveside00468@gmail.com>
2022-08-18 14:18:32 -0700

    Test

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,14 @@
 #ifndef "main.h"
 #define "main.h"
 
-int _putchar(char);
+/*
+ * File: main.h
+ * AUth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +17,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* _MAIN_H_ */
+#endif /* MAIN_H */
